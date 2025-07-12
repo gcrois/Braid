10a8ce7e006c3c65c1ca04f18b2a8b44638693bc
Gregory Croisdale <gregtc@umich.edu>
2025-07-12 15:20:33 -0400

    py type updates

--- packages/py_example/src/py_worker_wrapper.ts
+++ packages/py_example/src/py_worker_wrapper.ts
@@ -2,7 +2,6 @@ import { WorkerWrapper } from "@braid/utils";
 import type { PyCore } from "./py_core";
 
 export function initCoreWorker() {
-	// In rollup, we use a direct path to the worker file
 	const workerUrl = new URL("./py_worker.js", import.meta.url).href;
 	return new WorkerWrapper<PyCore>({ url: workerUrl });
 }
